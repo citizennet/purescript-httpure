14bb2caefc23b898d874ce80eefd917b43eb39c6
Connor Prussin <connor@prussin.net>
2017-07-20 23:03:12 -0700

    Fix broken docs (#52)

--- src/HTTPure/Server.purs
+++ src/HTTPure/Server.purs
@@ -14,13 +14,13 @@ import HTTPure.HTTPureM as HTTPureM
 import HTTPure.Request as Request
 import HTTPure.Response as Response
 
--- | The ResponseM type simply conveniently wraps up an HTTPure monad that
+-- | The ServerM type simply conveniently wraps up an HTTPure monad that
 -- | returns a Unit. This type is the return type of the HTTPure serve and
 -- | related methods.
 type ServerM e = HTTPureM.HTTPureM e Unit
 
--- | This function a method which takes a request and returns a ResponseM, an
--- | HTTP request, and an HTTP response. It runs the request, extracts the
+-- | This function takes a method which takes a request and returns a ResponseM,
+-- | an HTTP request, and an HTTP response. It runs the request, extracts the
 -- | Response from the ResponseM, and sends the Response to the HTTP Response.
 handleRequest :: forall e.
                  (Request.Request -> Response.ResponseM e) ->
@@ -33,7 +33,7 @@ handleRequest router request response = do
     EffClass.liftEff $ router req >>= Response.send response
   pure unit
 
--- | Given an options object, an function mapping Request to ResponseM, and an
+-- | Given an options object, a function mapping Request to ResponseM, and an
 -- | HTTPureM containing effects to run on boot, creates and runs a HTTPure
 -- | server.
 boot :: forall e.
