165e834de15e96ea7ca7ee415c417fa06507165e
Connor Prussin <connor@prussin.net>
2020-02-26 16:12:29 -0800

    Don't use nixjs for purescript

--- shell.nix
+++ shell.nix
@@ -1,12 +1,9 @@
 {
-  purescript ? "0.12.4",
-  nixjs ? fetchTarball "https://github.com/cprussin/nixjs/tarball/release-19.03",
   nixpkgs ? <nixpkgs>
 }:
 
 let
-  nixjs-overlay = import nixjs { inherit purescript; };
-  pkgs = import nixpkgs { overlays = [ nixjs-overlay ]; };
+  pkgs = import nixpkgs {};
 in
 
 pkgs.mkShell {
