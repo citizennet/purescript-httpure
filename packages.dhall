ab8c1fe88cc378e65d868aefca64c2bf468c908c
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2021-03-03 20:16:22 +0100

    Update to purescript 0.14 (#167)

--- packages.dhall
+++ packages.dhall
@@ -1,5 +1,5 @@
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.13.6-20200127/packages.dhall sha256:06a623f48c49ea1c7675fdf47f81ddb02ae274558e29f511efae1df99ea92fb8
+      https://github.com/purescript/package-sets/releases/download/psc-0.14.0-20210302/packages.dhall sha256:20cc5b89cf15433623ad6f250f112bf7a6bd82b5972363ecff4abf1febb02c50
 
 let overrides = {=}
 
--- src/HTTPure/Utils.purs
+++ src/HTTPure/Utils.purs
@@ -1,6 +1,5 @@
 module HTTPure.Utils
-  ( module Global
-  , encodeURIComponent
+  ( encodeURIComponent
   , replacePlus
   , urlDecode
   ) where
@@ -9,12 +8,10 @@ import Prelude
 
 import Data.Maybe as Maybe
 import Data.String as String
-import Global (decodeURIComponent) as Global
-import Global.Unsafe (unsafeEncodeURIComponent)
-
+import JSURI as JSURI
 
 encodeURIComponent :: String -> String
-encodeURIComponent = unsafeEncodeURIComponent
+encodeURIComponent s = Maybe.fromMaybe s $ JSURI.encodeURIComponent s
 
 
 replacePlus :: String -> String
@@ -24,4 +21,4 @@ replacePlus =
 
 urlDecode :: String -> String
 urlDecode s =
-  Maybe.fromMaybe s $ Global.decodeURIComponent s
+  Maybe.fromMaybe s $ JSURI.decodeURIComponent s
